f783aee8ea08c358d6296112f9fc6b967082248a
woodcutter1998 <jeric05908@gmail.com>
2023-03-01 18:59:50 -0600

    add module

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,9 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import os
+import pathlib
 import sys
+sys.path.insert(0, pathlib.Path(__file__).parents[2].resolve().as_posix())
 
 sys.path.insert(0, os.path.abspath("../../src"))
 
